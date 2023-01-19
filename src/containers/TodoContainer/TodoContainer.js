58e82117321c79ae80484c74e2affc7f34d55d08
Shavaine <shavaine.brown@chz.com>
2023-01-19 13:12:47 -0500

    Feat: Remove React Defaults and Add Todo Container

---
+++ src/containers/TodoContainer/TodoContainer.js
@@ -0,0 +1,13 @@
+
+
+const TodoContainer = () => {
+  return (
+    <div className="container">
+        <div className="row">
+            <h3 className="text-center">To-do List</h3>
+        </div>
+    </div>
+  )
+}
+
+export default TodoContainer
