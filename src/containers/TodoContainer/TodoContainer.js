006dfc1af38e788f894d3693fda7115991717d46
Shavaine <shavaine.brown@chz.com>
2023-01-19 13:29:13 -0500

    Feat: Add TodoList and Todo Components

--- src/containers/TodoContainer/TodoContainer.js
+++ src/containers/TodoContainer/TodoContainer.js
@@ -1,13 +1,20 @@
+import { useState } from "react"
+import TodoList from "../../components/TodoList/TodoList";
 
+const listOfTodos = ['take out the trash', 'vaccum the stairs', 'go for a walk'];
 
 const TodoContainer = () => {
-  return (
-    <div className="container">
-        <div className="row">
-            <h3 className="text-center">To-do List</h3>
+    const [todos, TodosList] = useState(listOfTodos);
+
+
+    return (
+        <div className="container">
+            <div className="row">
+                <h3 className="text-center">To-do List</h3>
+                <TodoList todos={todos} />
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default TodoContainer
